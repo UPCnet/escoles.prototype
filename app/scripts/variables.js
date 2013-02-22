468497fc63eefef2720468f703944bbe554736d0
Pilar Marinas <pilar.marinas@pc47194.ad.upcnet.es>
2013-02-22 14:04:53 +0100

    Disseny curs

---
+++ app/scripts/variables.js
@@ -0,0 +1,5 @@
+if (!window._MAXUI) {window._MAXUI = {}; }
+window._MAXUI.username = 'pilar.marinas'
+window._MAXUI.token = 'fd83ad432f20ad83ec59f1dafdc7a21e'
+window._MAXUI.server = 'http://rocalcom.upc.edu'
+window._MAXUI.grant = 'password'
